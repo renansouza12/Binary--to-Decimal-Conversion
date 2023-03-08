07267b63afe341052da086562b2b79e9e7b51bab
RenanSouza <renansouza0122@gmail.com>
2023-03-08 19:39:11 -0300

    functio backspace added

--- js/main.js
+++ js/main.js
@@ -1,32 +1,46 @@
 const buttons = document.querySelectorAll(".btn");
 const show_binary = document.getElementById("binary");
 const resultDecimal = document.getElementById("decimal");
-const btn_convert = document.querySelector('.convert');
-const btn_delete= document.querySelector(".delete")
-buttons.forEach((btn)=>{// add click for buttons with class btn
-    btn.addEventListener("click",()=>{
-        var valueBinary =  btn.innerHTML; 
-        show_binary.innerHTML += valueBinary;
-        text_binary = show_binary.value;
-        const decimalNumber = binaryToDecimal(text_binary);
-        resultDecimal.innerHTML = decimalNumber;
-    
-        function binaryToDecimal(valueBinary) {
-            let decimal = 0;
-            let base = 1;
-          
-            while (valueBinary !== 0) {
-              let lastDigit = valueBinary % 10;
-              valueBinary = Math.floor(valueBinary / 10);
-              decimal += lastDigit * base;
-              base *= 2;
-            }
-          
-            return decimal;
-          }
-          
-    })
-   
+const btn_delete = document.querySelector(".delete")
 
 
+buttons.forEach((btn) => {// add click for buttons with class btn
+  btn.addEventListener("click", () => {
+    show_binary.value += btn.innerHTML;
+
+    //convert binary to decimal and display result
+    const decimalNumber = binaryToDecimal(show_binary.value);
+    resultDecimal.innerHTML = decimalNumber;
+
+  })
 })
+//click event added to backspace icon
+btn_delete.addEventListener('click',()=>{
+  backspace(show_binary.value);
+
+ //convert binary to decimal and display result
+  const decimalNumber = binaryToDecimal(show_binary.value);
+    resultDecimal.innerHTML = decimalNumber;
+})
+
+//functio to remove last character from textarea element
+function backspace(){
+  show_binary.value = show_binary.value.slice(0, -1)
+}
+
+//function to convert binary to decimal
+function binaryToDecimal(valueBinary) {
+  let decimal = 0;
+  let base = 1;
+
+  while (valueBinary !== 0) {
+    let lastDigit = valueBinary % 10;
+    valueBinary = Math.floor(valueBinary / 10);
+    decimal += lastDigit * base;
+    base *= 2;
+  }
+
+  return decimal;
+}
+
+
