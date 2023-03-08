2fffe97afc3ca39d8cc5afd1d17eef09bed70e22
RenanSouza <renansouza0122@gmail.com>
2023-03-07 22:33:25 -0300

    function to convert binary to decimal added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
 const buttons = document.querySelectorAll(".btn");
-const binary = document.getElementById("binary");
-const decimal = document.getElementById("decimal")
+const show_binary = document.getElementById("binary");
+const resultDecimal = document.getElementById("decimal");
+const btn_convert = document.querySelector('.convert');
 buttons.forEach((btn)=>{
     btn.addEventListener("click",()=>{
-        binary.innerHTML += btn.innerHTML;
+        var valueBinary =  btn.innerHTML;
+        show_binary.innerHTML += valueBinary;
+        text_binary = show_binary.value;
+
+        btn_convert.onclick = ()=>{
+            const decimalNumber = binaryToDecimal(text_binary);
+            resultDecimal.innerHTML = decimalNumber;
+        }
+        function binaryToDecimal(valueBinary) {
+            let decimal = 0;
+            let base = 1;
+          
+            while (valueBinary !== 0) {
+              let lastDigit = valueBinary % 10;
+              valueBinary = Math.floor(valueBinary / 10);
+              decimal += lastDigit * base;
+              base *= 2;
+            }
+          
+            return decimal;
+          }
+          
     })
+    
 })
+
